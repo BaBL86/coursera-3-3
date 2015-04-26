d3474af74dcb7e29e8b25025ae406c04d426d59c
Алексей <babl1986@gmail.com>
2015-04-26 22:25:29 +0300

    Data set, Readme and 1-4 step of course project.

---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+# Merges the training and the test sets to create one data set.
+
+# Change this path to your correct working directory with 
+# Human Activity Recognition Using Smartphones Dataset
+# and this script.
+
+setwd('/home/babl/coursera/Getting_and_Cleaning_Data/cp/coursera-3-3/')
+
+x_train<-read.table("train/X_train.txt")
+x_test<-read.table("test/X_test.txt")
+x_set<-rbind(xtrain, xtest)
+
+subj_train<-read.table("train/subject_train.txt")
+subj_test<-read.table("test/subject_test.txt")
+subj_set <- rbind(subjtrain, subjtest)
+
+y_train<-read.table("train/y_train.txt")
+y_test<-read.table("test/y_test.txt")
+y_set<-rbind(ytrain, ytest)
+
+
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+features<-read.table("features.txt")
+features_mean_std <- features$V2 %like% '-mean\\(\\)' | features$V2 %like% '-std\\(\\)'
+x_set<-x_set[,features_mean_std]
+
+namesx<-features[features_mean_std, 2]
+namesx<-gsub("\\(|\\)", "", namesx)
+names(x_set)<-namesx
+
+
+# Uses descriptive activity names to name the activities in the data set
+
+activity_labels<-read.table("activity_labels.txt")
+activity_good_labels<-tolower(activity_labels[, 2])
+activity_good_labels<-gsub("_", "", activity_good_labels)
+activity_labels[, 2] = activity_good_labels
+y_set[,1] = activity_labels[y_set[,1], 2]
+names(y_set) <- "activity"
+
+
+# Appropriately labels the data set with descriptive variable names.
+
+names(subj_set) <- "subject"
+cleaned_set <- cbind(subj_set, y_set, x_set)
+write.table(cleaned_set, "merged_clean_set.txt")
+
+
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+#avg_data_set.txt
+
